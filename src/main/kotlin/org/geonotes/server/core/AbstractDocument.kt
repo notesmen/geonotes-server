8cdfa9429d7ef7f977ed90e40fa36c611733c884
Alexey Filinovich <aliaks35@gmail.com>
2020-11-19 13:46:59 +0300

    Small bug fix

--- src/main/kotlin/org/geonotes/server/core/AbstractDocument.kt
+++ src/main/kotlin/org/geonotes/server/core/AbstractDocument.kt
@@ -9,10 +9,7 @@ open class AbstractDocument(@Id var id: ObjectId = ObjectId.get()) {
         if (this === other) {
             return true
         }
-        if (this.javaClass != other?.javaClass) {
-            return false
-        }
-        return this.id == (other as AbstractDocument).id
+        return this.id == (other as AbstractDocument?)?.id
     }
 
     override fun hashCode(): Int = id.hashCode()
